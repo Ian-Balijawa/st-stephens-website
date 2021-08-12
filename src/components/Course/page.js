ebddc4433f3de07f50682f13b88af6160616d636
Bkroland19 <rkbukenya@cis.mak.ac.ug>
2021-08-12 17:18:27 +0300

    error correction

--- src/components/Course/page.js
+++ src/components/Course/page.js
@@ -6,23 +6,21 @@ import Gallery from "./gallery";
 import Card from "./card";
 import { Registration } from "../Registration";
 import image from "../../assets/john-jennings.jpg";
-import {courseData} from '../../config/data';
-import styles from './styles';
+import styles from "./styles";
 
-const CoursePage = ({pageTitle}) => (
-  <div>
-    <Hero title1={pageTitle} title2="Design" linkText="Register" image={image} />
-    <AnimatedSection>
-      <div
-        className={styles.pageContainer}>
-        <Summary summaryItems={courseData.summaryItems} />
-        <Card text={courseData.aboutCourse} title={`About the course`} />
-        <Gallery projects={courseData.projects} />
-        <Card {...courseData.teacherCard} />
-      </div>
-    </AnimatedSection>
-    <Registration />
-  </div>
+const CoursePage = ({ pageTitle }) => (
+	<div>
+		<Hero
+			title1={pageTitle}
+			title2="Design"
+			linkText="Register"
+			image={image}
+		/>
+		<AnimatedSection>
+			<div className={styles.pageContainer}></div>
+		</AnimatedSection>
+		<Registration />
+	</div>
 );
 
 export default CoursePage;
--- src/config/data.js
+++ src/config/data.js
@@ -1,24 +1,7 @@
 const summaryItems = [
-    { title: "performance", info: "No statistics yet" },
-    { title: "directions", info: "" },
-    { title: "reviews", info: "No reviews yet" },
+	{ title: "performance", info: "No statistics yet" },
+	{ title: "directions", info: "" },
+	{ title: "reviews", info: "No reviews yet" },
 ];
-  const aboutSchool ="St. Stephen’s College  Bajja is built on a catholic foundation but admits students of all faiths. It is a private school with a current ratio of 1:1.5 boys to girls. It offers both sciences and arts. The school, though on a catholic foundation, admits students of all faiths.The school has great teachers who are capable of inspiring other people and we encourage you to use this chance of educating your son or daughter at St. Stephen’s College Bajja. Teachers really care.",
-  
-  const admissions = "The offices at the school and at Namuwongo are always open for registration of new students except on 22nd December to 2nd January. Our school fees structure is affordable. However we argue you to pay on time to avoid inconveniences.",
-  
-  const co-curricularActivities = {
-    imgSrc: "sports.jpg",
-    text:
-      "The school has a variety of games and sports for the students to keep fit and they include; Football for both boys and girls, Netball, Volley ball for both boys and girls, Table Tennis, Scrabble, Chess and Athletics.",
-  };
 
-  const courseData = {
-    summaryItems,
-    aboutSchool,
-    admissions,
-    co-curricularActivities
-  }
-
-
-export {courseData}
+export { summaryItems };
